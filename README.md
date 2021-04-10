2fb3c04493fde6d22e6f48ce995cfb104a9fd374
Tim Vidas <tvidas@alumni.cmu.edu>
2021-04-10 16:01:03 -0500

    adds minimum hotkey functionality and README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# hacker-hotkey
-Hacker Hotkey shipped firmware
+# Hacker HotKey
+This repos contains example software for the [Kernelcon](https://kernelcon.org/) 2021 Hack Live! badge - the Hacker HotKey.
+
+[![hacker hotkey hotkey image](./hackerhotkey-sm-white.png)](https://kernelcon.square.site/)
+
+## Compiling and uploading firmware
+
+1. connect micro USB cable  
+   * needs to be a data cable, not just a charging/power cable
+2. open aduino IDE  
+   * install if necessary 
+       * https://www.arduino.cc/en/Guide
+   * install keyboard library if necessary:
+       * tools -> Manage Libraries -> search "keyboard" -> install
+   ```sh
+    arduino hackerHotKey.ino
+   ```
+3. select "Arduino Leonardo" board type
+   * install Arduino boards if necessary:
+      * tools -> Boards Manager -> search "Arduino AVR Boards" -> install 
+4. select com port
+   * tools -> port
+5. Verify port comms work
+   * tools -> "Get Board Info"
+   * should return something like
+      ```sh
+      BN: Arduino Leonardo
+      VID: 0x2341
+      PID: 0x8036
+      SN: Upload any sketch to obtain it
+      ```
+5. upload code
+   * sketch -> upload
+   * Arduino IDE will compile and upload firmware to the Hacker Hotkey, which you can use immediately after upload (no unplugging, reseting, etc required)
+
+
---
+++ hackerHotKey/hackerHotKey.ino
@@ -0,0 +1,68 @@
+
+/*  Hacker HotKey sample firmware
+ *   
+ *  Copyright 2021 Kernelcon
+ *  SPDX-License-Identifier: Apache-2.0
+ *   
+ *  Button layout is top to bottom, left to right.  So..
+ *  1 2 3 4
+ *  5 6 7 8
+ */
+
+#include <Keyboard.h>
+
+#define BTN1  13
+#define BTN2  5
+#define BTN3  10
+#define BTN4  9
+#define BTN5  8
+#define BTN6  6
+#define BTN7  12
+#define BTN8  4
+
+void setup() {
+  pinMode(BTN1, INPUT_PULLUP);
+  pinMode(BTN2, INPUT_PULLUP);
+  pinMode(BTN3, INPUT_PULLUP);
+  pinMode(BTN4, INPUT_PULLUP);
+  pinMode(BTN5, INPUT_PULLUP);
+  pinMode(BTN6, INPUT_PULLUP);
+  pinMode(BTN7, INPUT_PULLUP);
+  pinMode(BTN8, INPUT_PULLUP);
+  Keyboard.begin();
+}
+
+void loop() {
+  if (digitalRead(BTN1) == LOW) {
+    Keyboard.println("https://kernelcon.org");
+    delay(200);
+  }
+  else if (digitalRead(BTN2) == LOW) {
+    Keyboard.println("https://discord.kernelcon.org");
+    delay(200);
+  }
+  else if (digitalRead(BTN3) == LOW) {
+    Keyboard.println("https://twitch.kernelcon.org");
+    delay(200);
+  }
+  else if (digitalRead(BTN4) == LOW) {
+    Keyboard.println("https://github.com/kernelcon");
+    delay(200);
+  }
+  else if (digitalRead(BTN5) == LOW) {
+    Keyboard.println("!vote 4");
+    delay(200);
+  }
+  else if (digitalRead(BTN6) == LOW) {
+    Keyboard.println("!vote 3");
+    delay(200);
+  }
+  else if (digitalRead(BTN7) == LOW) {
+    Keyboard.println("!vote 2");
+    delay(200);
+  }
+  else if (digitalRead(BTN8) == LOW) {
+    Keyboard.println("!vote 1");
+    delay(200);
+  }
+}
